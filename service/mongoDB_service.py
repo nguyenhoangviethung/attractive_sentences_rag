0ab524714cbc12ffa1559c5be03a313da82eb5da
nguyenhoangviethung <nguyenhoangviethung@gmail.com>
2025-05-30 10:37:17 +0700

    Add middleware for check permission

--- service/mongoDB_service.py
+++ service/mongoDB_service.py
@@ -55,57 +55,74 @@ def get_sentences(is_authorized):
             "message": "Server Error"
         }), 500
 
+from flask import jsonify
+from pymongo.errors import BulkWriteError
+import os
+from utilities.middleware import MiddleWare as MW
+
 @MW.check_permission
 def pre_data(is_authorized):
-    if(is_authorized):
-        sorted_images_unsupervise = cs.get_all_images(CONFIG["FOLDER_CLOUD_UNSUPERVISE"])
-
-        sorted_images_supervise = cs.get_all_images(CONFIG["FOLDER_CLOUD_SUPERVISE"])
-
-        from imageCollector.readImage import process_unsupervised_images, process_supervised_images
-        i1, i2 = get_config()
-        print(len(sorted_images_unsupervise))
-        print(len(sorted_images_supervise))
-        for i, item in enumerate(sorted_images_supervise[i2::]):
-            cs.download_image(item["secure_url"], CONFIG["FOLDER_PATH_SUPERVISE"] + f'/img{i+i2+1}' + '.jpg')
-        for i, item in enumerate(sorted_images_unsupervise[i1::]):
-            cs.download_image(item["secure_url"], CONFIG["FOLDER_PATH_UNSUPERVISE"] + f'/img{i+i1+1}' + '.jpg')
-        unsupervised, index_1 = process_unsupervised_images(
-            config = CONFIG, 
-            model_name='gemini-2.0-flash',
-            START_INDEX_UNSUPERVISE =  i1
-        )
-        supervised, index_2 = process_supervised_images(
-            config = CONFIG,
-            model_name='gemini-2.0-flash',
-            START_INDEX_SUPERVISE = i2
-        )
-        results = unsupervised + supervised
-        try:        
-            config = ConfigData(START_INDEX_SUPERVISE=index_2, START_INDEX_UNSUPERVISE=index_1)
-            db.configData.replace_one({}, config.to_dict(), upsert=True)
-            documents = [SentenceData(item["keyword"], item["text"]).to_dict() for item in results]
-            db.sentences.insert_many(documents, ordered = False)
-
-            return jsonify({
-                "message": "Sentences added"
-            }), 200
-        except BulkWriteError as e:
-            return jsonify({
-                "message": "Sentences added"
-            }), 200
-        
-        except Exception as e:
-            if str(e) == "documents must be a non-empty list":
-                return jsonify({
-                    "message": "Sentences added"
-                }), 200
-            print(e)
-            return jsonify({
-                "message": "Sentence isn't added"
-            }), 500
-    else:
+    if not is_authorized:
         return jsonify({
             "message": "Have not permission"
         }), 401
 
+    try:
+        # Đảm bảo thư mục lưu ảnh local tồn tại
+        os.makedirs(CONFIG["FOLDER_PATH_UNSUPERVISE"], exist_ok=True)
+        os.makedirs(CONFIG["FOLDER_PATH_SUPERVISE"], exist_ok=True)
+        os.makedirs("data", exist_ok=True)
+
+        # Lấy danh sách ảnh từ Cloudinary
+        sorted_images_unsupervise = cs.get_all_images(CONFIG["FOLDER_CLOUD_UNSUPERVISE"])
+        sorted_images_supervise = cs.get_all_images(CONFIG["FOLDER_CLOUD_SUPERVISE"])
+
+        from imageCollector.readImage import process_unsupervised_images, process_supervised_images
+        i1, i2 = get_config()
+
+        print(len(sorted_images_unsupervise))
+        print(len(sorted_images_supervise))
+
+        # Tải ảnh về local
+        for i, item in enumerate(sorted_images_supervise[i2:]):
+            cs.download_image(item["secure_url"], f'{CONFIG["FOLDER_PATH_SUPERVISE"]}/img{i+i2+1}.jpg')
+        for i, item in enumerate(sorted_images_unsupervise[i1:]):
+            cs.download_image(item["secure_url"], f'{CONFIG["FOLDER_PATH_UNSUPERVISE"]}/img{i+i1+1}.jpg')
+
+        # Xử lý ảnh
+        unsupervised, index_1 = process_unsupervised_images(
+            config=CONFIG,
+            model_name='gemini-2.0-flash',
+            START_INDEX_UNSUPERVISE=i1
+        )
+        supervised, index_2 = process_supervised_images(
+            config=CONFIG,
+            model_name='gemini-2.0-flash',
+            START_INDEX_SUPERVISE=i2
+        )
+
+        results = unsupervised + supervised
+
+        config = ConfigData(
+            START_INDEX_SUPERVISE=index_2,
+            START_INDEX_UNSUPERVISE=index_1
+        )
+        db.configData.replace_one({}, config.to_dict(), upsert=True)
+
+        documents = [SentenceData(item["keyword"], item["text"]).to_dict() for item in results]
+
+        if not documents:
+            return jsonify({"message": "No sentences to add"}), 200
+
+        db.sentences.insert_many(documents, ordered=False)
+
+        return jsonify({"message": "Sentences added"}), 200
+
+    except BulkWriteError:
+        return jsonify({"message": "Sentences added"}), 200
+
+    except Exception as e:
+        if "documents must be a non-empty list" in str(e):
+            return jsonify({"message": "Sentences added"}), 200
+        print(e)
+        return jsonify({"message": "Sentence isn't added"}), 500
